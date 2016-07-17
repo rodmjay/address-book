274add7f5bee4b6446953e8a847ad383ba6de52a
Rod Johnson <rodmjay@gmail.com>
2016-07-17 11:28:54 -0600

    javascript infrastructure

--- address-book/app/AddressBook.jsx
+++ address-book/app/AddressBook.jsx
@@ -1,1 +1,102 @@
-﻿console.log('app');
+﻿(function () {
+
+    var contactService = (function() {
+        return{
+            save: function(contact, name, phoneNumber) {
+                console.log('contact', contact);
+            },
+            create: function(name, phoneNumber) {
+                console.log('create',
+                {
+                    name: name,
+                    phoneNumber: phoneNumber
+                });
+            }
+        }
+    })();
+
+    var AddressBook = React.createClass({
+        getInitialState:function() {
+            return {
+                contacts:[]
+            }
+        },
+        componentDidMount: function () {
+            this.serverRequest = $.get('/api/contacts', function (result) {
+                this.setState({
+                    contacts: result
+                });
+            }.bind(this));
+        },
+        render: function () {
+            return (
+ <div className="row">
+    <div className="col-md-4">
+        <CreateContact />
+    </div>
+    <div className="col-md-8">
+        <ContactList contacts={this.state.contacts} />
+    </div>
+</div>);
+        }
+    });
+
+    var CreateContact = React.createClass({
+        getInitialState: function() {
+            return {
+                name: '',
+                phoneNumber: ''
+            }
+        },
+        save: function (e) {
+            contactService.save(this.state.name, this.state.phoneNumber);
+            this.setState({ name: '', phoneNumber: '' });
+            e.preventDefault();
+        },
+        handlePhoneNumberChange: function (e) {
+            this.setState({ phoneNumber: e.target.value });
+        },
+        handleNameChange: function (e) {
+            this.setState({ name: e.target.value });
+        },
+        render: function () {
+            return (
+<form onSubmit={this.save}>
+    <div className="form-group">
+    <input placeholder="Name"
+           type="text"
+           value={this.state.name}
+           onChange={this.handleNameChange}
+           className="form-control" />
+    </div>
+    <div className="form-group">
+    <input placeholder="Phone"
+           type="text"
+           value={this.state.phoneNumber}
+           onChange={this.handlePhoneNumberChange}
+           className="form-control" />
+    </div>
+      <button type="submit" className="btn btn-default">Create Contact</button>
+</form>);
+        }
+    });
+
+    var ContactList = React.createClass({
+
+        render: function () {
+            var results = this.props.contacts;
+            return (
+              <ol>
+                {results.map(function(result) {
+                    return <li key={result.name}>{result.name}</li>;
+                })}
+              </ol>
+            );
+        }
+    });
+
+    ReactDOM.render(
+			<AddressBook />,
+			document.getElementById('addressbook')
+		);
+})();
---
+++ address-book/app/contact-model.js
@@ -0,0 +1,52 @@
+﻿var app = app || {};
+(function () {
+    'use strict';
+
+    var Utils = app.Utils;
+
+    app.ContactModel = function (key) {
+        this.key = key;
+        this.contacts = Utils.store(key);
+        this.onChanges = [];
+    };
+
+    app.ContactModel.prototype.subscribe = function (onChange) {
+        this.onChanges.push(onChange);
+    };
+
+    app.ContactModel.prototype.inform = function () {
+        Utils.store(this.key, this.contacts);
+        this.onChanges.forEach(function (cb) { cb(); });
+    };
+
+    app.ContactModel.prototype.addContact = function (name, phone) {
+        this.contacts = this.contacts.concat({
+            id: Utils.uuid(),
+            name: name,
+            phoneNumber: phone
+        });
+
+        this.inform();
+    };
+
+
+    app.ContactModel.prototype.destroy = function (contact) {
+        this.contacts = this.contacts.filter(function (candidate) {
+            return candidate !== contact;
+        });
+
+        this.inform();
+    };
+
+    app.ContactModel.prototype.save = function (contactToSave, name, phone) {
+        this.contacts = this.contacts.map(function (contact) {
+            return contact !== contactToSave ? contact : {
+                name: name,
+                phoneNumber: phone
+            };
+        });
+
+        this.inform();
+    };
+
+})();
