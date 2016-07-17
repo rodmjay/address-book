b17c31459f7ff92a5b8849602acad6f39c491f34
Rod Johnson <rodmjay@gmail.com>
2016-07-17 09:44:19 -0600

    Added nuget pakages

---
+++ address-book/BrowserJsonFormatter.cs
@@ -0,0 +1,30 @@
+﻿#region
+
+using System;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+#endregion
+
+namespace AddressBook
+{
+    public class BrowserJsonFormatter : JsonMediaTypeFormatter
+    {
+        public BrowserJsonFormatter()
+        {
+            SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+            MediaTypeMappings.Add(new RequestHeaderMapping("Accept", "text/html", StringComparison.InvariantCultureIgnoreCase,
+                true, "application/json"));
+            SerializerSettings.Formatting = Formatting.Indented;
+            SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        }
+
+        public override void SetDefaultContentHeaders(Type type, HttpContentHeaders headers, MediaTypeHeaderValue mediaType)
+        {
+            base.SetDefaultContentHeaders(type, headers, mediaType);
+            headers.ContentType = new MediaTypeHeaderValue("application/json");
+        }
+    }
+}
---
+++ address-book/Startup.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Http;
+using System.Web.Http.Cors;
+using AddressBook;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace AddressBook
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            var cors = new EnableCorsAttribute(origins: "*", headers: "*", methods: "*");
+            config.EnableCors(cors);
+
+            config.MapHttpAttributeRoutes();
+            config.Formatters.Clear();
+            config.Formatters.Add(new BrowserJsonFormatter());
+            app.UseWebApi(config);
+        }
+    }
+}
