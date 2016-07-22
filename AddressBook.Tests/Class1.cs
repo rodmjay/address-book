4827204983ce7abd202f44c426f7102f12e88d71
Rod Johnson <rodmjay@gmail.com>
2016-07-21 19:17:03 -0600

    unit tests for TheGetContactsMethod

--- AddressBook.Tests/Class1.cs
+++ AddressBook.Tests/Class1.cs
@@ -1,12 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Web;
+using AddressBook.Models;
+using AddressBook.Service;
+using Moq;
+using NUnit.Framework;
 
 namespace AddressBook.Tests
 {
-    public class Class1
+    [TestFixture]
+    public class SessionContactServiceTests
     {
+        protected MockHttpSession SessionStateBase;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            SessionStateBase = new MockHttpSession();
+        }
+
+        protected SessionContactService GetService()
+        {
+            return new SessionContactService(SessionStateBase);
+        }
+
+        [TestFixture]
+        public class TheGetContactsMethod : SessionContactServiceTests
+        {
+            [Test]
+            public void ReturnsCorrectNumberOfContacts()
+            {
+                List<ContactModel> contacts = new List<ContactModel>
+                {
+                    new ContactModel()
+                };
+
+                SessionStateBase.Add("Contacts", contacts);
+
+                var service = GetService();
+
+                Assert.AreEqual(1, service.GetContacts().Count());
+            } 
+        }
+
+        [TestFixture]
+        public class TheUpdateContactMethod : SessionContactServiceTests
+        {
+            
+        }
+
+        [TestFixture]
+        public class TheCreateContactMethod : SessionContactServiceTests
+        {
+            
+        }
     }
 }
---
+++ AddressBook.Tests/MockHttpSession.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Web;
+
+namespace AddressBook.Tests
+{
+    public class MockHttpSession : HttpSessionStateBase
+    {
+        Dictionary<string, object> m_SessionStorage = new Dictionary<string, object>();
+
+        public override object this[string name]
+        {
+            get { return m_SessionStorage[name]; }
+            set { m_SessionStorage[name] = value; }
+        }
+
+        public override void Add(string name, object value)
+        {
+            m_SessionStorage.Add(name, value);
+        }
+    }
+}
