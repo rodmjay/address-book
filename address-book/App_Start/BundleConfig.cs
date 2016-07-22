f341eca170b0f698bfaf1a369af13807209f1281
Rod Johnson <rodmjay@gmail.com>
2016-07-22 08:06:14 -0600

    bundle jasmine assets

--- address-book/App_Start/BundleConfig.cs
+++ address-book/App_Start/BundleConfig.cs
@@ -10,6 +10,11 @@ namespace AddressBook
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jasmine").Include(
+                "~/Scripts/jasmine/jasmine.js", 
+                "~/Scripts/jasmine/jasmine-html.js",
+                "~/Scripts/jasmine/boot.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
