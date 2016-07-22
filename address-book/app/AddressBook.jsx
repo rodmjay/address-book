471b6ac474bc9f11834dd5dd3e32d2368a6f3557
Rod Johnson <rodmjay@gmail.com>
2016-07-21 20:54:46 -0600

    created unit tests with jasmine

--- address-book/app/AddressBook.jsx
+++ address-book/app/AddressBook.jsx
@@ -1,64 +1,59 @@
-﻿(function () {
-
-    var AddressBook = React.createClass({
-        getInitialState: function () {
-            return {
-                contacts: []
-            }
-        },
-        componentDidMount: function () {
-            $.get('/api/contacts', function (result) {
-                this.setState({
-                    contacts: result
-                });
-            }.bind(this));
-        },
-        destroy: function (id) {
-
-            $.ajax({
-                url: "/api/contacts/" + id,
-                type: "DELETE",
-                dataType: "json",
-                success: function () {
-                    //console.log('deleted');
-                }.bind(this)
-            });
-
+﻿
+var AddressBook = React.createClass({
+    getInitialState: function () {
+        return {
+            contacts: []
+        }
+    },
+    componentDidMount: function () {
+        $.get('/api/contacts', function (result) {
             this.setState({
-                contacts: this.state.contacts.filter((_, i) => i !== id)
+                contacts: result
             });
+        }.bind(this));
+    },
+    destroy: function (id, successFn) {
+        successFn = successFn || function () { };
+        $.ajax({
+            url: "/api/contacts/" + id,
+            type: "DELETE",
+            dataType: "json",
+            success: successFn
+        });
 
-        },
-        update: function(id, contact) {
-            $.ajax({
-                url: "/api/contacts/"+id,
-                type: "POST",
-                dataType: "json",
-                contentType: "application/json",
-                data: JSON.stringify(contact),
-                success: function () {
-                    //console.log('saved');
-                }.bind(this)
-            });
-        },
-        create: function (contact) {
-            $.ajax({
-                url: "/api/contacts",
-                type: "POST",
-                dataType: "json",
-                contentType: "application/json",
-                data: JSON.stringify(contact),
-                success: function () {
-                    //console.log('saved');
-                }.bind(this)
-            });
+        this.setState({
+            contacts: this.state.contacts.filter((_, i) => i !== id)
+        });
 
-            this.setState({
-                contacts: this.state.contacts.concat([contact])
-            });
-        },
-        render: function () {
-            return (
+    },
+    update: function (id, contact, successFn) {
+        successFn = successFn || function () { };
+        $.ajax({
+            url: "/api/contacts/" + id,
+            type: "POST",
+            dataType: "json",
+            contentType: "application/json",
+            data: JSON.stringify(contact),
+            success: successFn
+        });
+    },
+    create: function (contact, successFn) {
+        successFn = successFn || function(){};
+        $.ajax({
+            url: "/api/contacts",
+            type: "POST",
+            dataType: "json",
+            contentType: "application/json",
+            data: JSON.stringify(contact),
+            success: successFn
+        });
+
+        this.setState({
+            contacts: this.state.contacts.concat([contact])
+        });
+    },
+    render: function () {
+        return (
  <div className="row">
     <div className="col-md-3">
         <div className="well">
@@ -78,42 +73,42 @@
                       {this.state.contacts.map(function (result, i) {
                           return (
                               <Contact key={i}
-                                       id={i} 
-                                       contact={result} 
+                                       id={i}
+                                       contact={result}
                                        onUpdate={this.update}
-                                       onDestroy={this.destroy}/>);
+                                       onDestroy={this.destroy} />);
                       }.bind(this))}
                   </tbody>
 
         </table>
     </div>
 
-</div>);
+ </div>);
+    }
+});
+
+
+
+var CreateContact = React.createClass({
+    getInitialState: function () {
+        return {
+            name: '',
+            phoneNumber: ''
         }
-    });
-
-  
-
-    var CreateContact = React.createClass({
-        getInitialState: function() {
-            return {
-                name: '',
-                phoneNumber: ''
-            }
-        },
-        handleSubmit: function (e) {
-            e.preventDefault();
-            this.props.onCreate(this.state);
-            this.setState(this.getInitialState());
-        },
-        handlePhoneNumberChange: function (e) {
-            this.setState({ phoneNumber: e.target.value });
-        },
-        handleNameChange: function (e) {
-            this.setState({ name: e.target.value });
-        },
-        render: function () {
-            return (
+    },
+    handleSubmit: function (e) {
+        e.preventDefault();
+        this.props.onCreate(this.state);
+        this.setState(this.getInitialState());
+    },
+    handlePhoneNumberChange: function (e) {
+        this.setState({ phoneNumber: e.target.value });
+    },
+    handleNameChange: function (e) {
+        this.setState({ name: e.target.value });
+    },
+    render: function () {
+        return (
 <form onSubmit={this.handleSubmit}>
     <div className="form-group">
     <input placeholder="Name"
@@ -129,36 +124,37 @@
            onChange={this.handlePhoneNumberChange}
            className="form-control" />
     </div>
-      <button 
-              type="submit" 
-              className="btn btn-primary">Create Contact</button>
+      <button type="submit"
+              className="btn btn-primary">
+          Create Contact
+      </button>
 </form>);
-        }
-    });
+    }
+});
 
-    var Contact = React.createClass({
-        handleUpdateButtonClick:function(e) {
-            this.props.onUpdate(this.props.id, this.state);
-            e.preventDefault();
-        },
-        handleDeleteButtonClick: function (e) {
-            this.props.onDestroy(this.props.id);
-            e.preventDefault();
-        },
-        handlePhoneNumberChange: function (e) {
-            this.setState({ phoneNumber: e.target.value });
-        },
-        handleNameChange: function (e) {
-            this.setState({ name: e.target.value });
-        },
-        getInitialState:function() {
-            return{
-                name: this.props.contact.name,
-                phoneNumber: this.props.contact.phoneNumber
-            }
-        },
-        render: function () {
-            return (
+var Contact = React.createClass({
+    handleUpdateButtonClick: function (e) {
+        this.props.onUpdate(this.props.id, this.state);
+        e.preventDefault();
+    },
+    handleDeleteButtonClick: function (e) {
+        this.props.onDestroy(this.props.id);
+        e.preventDefault();
+    },
+    handlePhoneNumberChange: function (e) {
+        this.setState({ phoneNumber: e.target.value });
+    },
+    handleNameChange: function (e) {
+        this.setState({ name: e.target.value });
+    },
+    getInitialState: function () {
+        return {
+            name: this.props.contact.name,
+            phoneNumber: this.props.contact.phoneNumber
+        }
+    },
+    render: function () {
+        return (
               <tr>
                 <th><input type="text" onChange={this.handleNameChange} className="form-control" value={this.state.name} /></th>
                 <td><input type="text" onChange={this.handlePhoneNumberChange} className="form-control" value={this.state.phoneNumber} /></td>
@@ -168,11 +164,10 @@
                 </td>
               </tr>
             );
-        }
-    });
+    }
+});
 
-    ReactDOM.render(
+ReactDOM.render(
 			<AddressBook />,
 			document.getElementById('addressbook')
 		);
-})();
--- address-book/specs/AddressBook-spec.jsx
+++ address-book/specs/AddressBook-spec.jsx
@@ -1,1 +1,54 @@
-﻿console.log('addressbook spec');
+﻿describe("AddressBook", function () {
+    var instance;
+    var container = document.getElementById('addressbook');
+
+    beforeEach(function () {
+        instance = ReactDOM.render(<AddressBook />, container);
+        console.log(instance);
+
+        // crush the old contacts
+        for (var i = 0; i < instance.state.contacts.length; i++) {
+            instance.destroy(i);
+        }
+    });
+
+    it("should be able to create a contact", function () {
+        var count = instance.state.contacts.length;
+        instance.create({
+            name: 'test',
+            phoneNumber: 'test2'
+        });
+        expect(instance.state.contacts.length).toEqual(count + 1);
+    });
+
+    it('should be able to update a contact',
+        function() {
+            instance.create({
+                name: 'test',
+                phoneNumber: 'test2'
+            },function() {
+                instance.update(0,
+                {
+                    name: 'updated',
+                    phoneNumber: 'updated2'
+                },function() {
+                });
+            });
+            expect(instance.state.contacts[0].name).toEqual('updated');
+
+        });
+
+    it('should be able to delete a contact',
+       function () {
+           instance.create({
+               name: 'test',
+               phoneNumber: 'test2'
+           }, function () {
+               instance.destroy(0, function() {
+               });
+           });
+           expect(instance.state.contacts.length).toEqual(0);
+
+       });
+
+});
